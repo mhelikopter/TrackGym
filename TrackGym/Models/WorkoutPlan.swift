871fde72d055b6197aacec184feaa84efa14f151
Maximilian Ehling <maximilian.ehling@smail.th-koeln.de>
2026-05-13 23:09:26 +0200

    fix(models): make WorkoutPlan.exercises deleteRule explicit
    Fixes MHE-10

--- TrackGym/Models/WorkoutPlan.swift
+++ TrackGym/Models/WorkoutPlan.swift
@@ -4,6 +4,7 @@ import SwiftData
 @Model
 final class WorkoutPlan {
     var name: String
+    @Relationship(deleteRule: .nullify, inverse: \Exercise.workoutPlans)
     var exercises: [Exercise] = []
 
     init(name: String) {
